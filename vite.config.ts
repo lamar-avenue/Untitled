9863343ed95780d704f5b72765c85301189cdb23
scrimmy <52431805+lamar-avenue@users.noreply.github.com>
2026-02-25 04:03:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-
   plugins: [react()],
   base: "/Untitled/",
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
-
-  base: '/Untitled/',
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-
-  },
 });
